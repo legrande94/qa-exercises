42b6e26b134228d7880da1d96b78513cb1975678
legrande94 <anishare.legrande@gmail.com>
2026-06-05 13:27:05 +0300

    some fixes

--- src/main/java/common/CommonAction.java
+++ src/main/java/common/CommonAction.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
+
 import static common.Config.IMPLICIT_WAIT;
 
 public class CommonAction {
@@ -17,11 +18,26 @@ public class CommonAction {
 
     public static WebDriver createDriver() throws MalformedURLException {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
+            try {
+                ChromeOptions options = new ChromeOptions();
 
-            driver = new RemoteWebDriver(URI.create("http://selenoid:4444/wd/hub").toURL(), options);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
+                options.setCapability("browserName", "chrome");
+                options.setCapability("selenoid:options", Map.of(
+                        "enableVNC", true,
+                        "enableVideo", false
+                ));
+
+                driver = new RemoteWebDriver(
+                        URI.create("http://selenoid:4444/wd/hub").toURL(),
+                        options
+                );
+
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
+
+            } catch (Exception e) {
+                throw new RuntimeException("Driver init failed", e);
+            }
         }
         return driver;
     }
