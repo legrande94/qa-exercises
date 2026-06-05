9edb3e14ac50a69a47d6cf253cbf4bec990eb3d9
legrande94 <anishare.legrande@gmail.com>
2026-06-05 13:14:24 +0300

    connect to selenoid

--- src/main/java/common/CommonAction.java
+++ src/main/java/common/CommonAction.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
 import static common.Config.IMPLICIT_WAIT;
@@ -18,7 +19,7 @@ public class CommonAction {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
 
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),options);
+            driver = new RemoteWebDriver(URI.create("http://selenoid:4444/wd/hub").toURL(), options);
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
         }
